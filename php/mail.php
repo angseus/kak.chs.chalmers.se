4d8cad205aa57e55e955c42aa011edc5538aa342
Axel Niklasson <axel.niklasson@live.com>
2015-11-16 19:35:13 +0100

    Updated mail script

--- php/mail.php
+++ php/mail.php
@@ -1,9 +1,9 @@
 <?php
 $to      = "info@kak.chs.chalmers.se";
-$subject = "Nytt meddelande från hemsideformuläret";
+$subject = "Nytt meddelande";
 $message = $_POST["message"] . "\r\n";
 
-$headers = "Content-type: text/plain; charset=utf-8\r\n";
+$headers = "Content-Type: text/html; charset=UTF-8\r\n";
 $headers .= "From: " . $_POST["name"] . " <" . $_POST["email"] . ">\r\n";
 
 mail($to, $subject, $message, $headers);
